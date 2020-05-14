70fb0f592a4edffd2b8bc7448e28e5e69b1df668
AnnieMarkina <anniemurkina@gmail.com>
2020-05-14 12:43:20 +0300

    Updates

---
+++ precache-manifest.933052203c8da3ab4c5ccd8cdbe6edb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccead7b3115d072cde417b7794d0e43",
+    "url": "./index.html"
+  },
+  {
+    "revision": "efd72b299239baa23734",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f77ad318a4fec3bc0654",
+    "url": "./static/js/2.8631adee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8631adee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd72b299239baa23734",
+    "url": "./static/js/main.6bafaa6d.chunk.js"
+  },
+  {
+    "revision": "cc951e334f15251cfce1",
+    "url": "./static/js/runtime-main.88190073.js"
+  }
+]);
